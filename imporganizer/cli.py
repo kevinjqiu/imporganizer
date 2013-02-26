153ce326957f9a57aeefab6d4ae134095db72f61
Kevin Qiu <kevin@freshbooks.com>
2013-02-25 22:00:03 -0500

    Use rope.

--- imporganizer/cli.py
+++ imporganizer/cli.py
@@ -1,89 +1,28 @@
 import argparse
-import ast
+import os
 
-from collections import namedtuple
-
-
-Name = namedtuple(
-    'Name',
-    ['name', 'asname'],
-)
-
-Import = namedtuple(
-    'Import',
-    ['node', 'names'],
-)
-
-
-ImportFrom = namedtuple(
-    'ImportFrom',
-    ['node', 'module', 'names', 'level'],
-)
-
-
-class MyVisitor(ast.NodeVisitor):
-    def __init__(self, root_node):
-        self.root_node = root_node
-        self.imports = []
-
-    def is_top_level_node(self, node):
-        return node in self.root_node.body
-
-    def visit_Import(self, node):
-        if self.is_top_level_node(node):
-            self.imports.append(
-                Import(
-                    node=node,
-                    names=[
-                        Name(n.name, n.asname)
-                        for n in node.names
-                    ]))
-
-    def visit_ImportFrom(self, node):
-        if self.is_top_level_node(node):
-            self.imports.append(
-                ImportFrom(
-                    node=node,
-                    module=node.module,
-                    names=[
-                        Name(
-                            name=name.name,
-                            asname=name.asname)
-                        for name in node.names
-                    ],
-                    level=node.level,
-                )
-            )
-
-
-class ImportOrganizer(object):
-    def __init__(self, stream):
-        self.stream = stream
-        self.root_node = ast.parse(self.stream.read())
-
-    def _collect_imports(self, visitor):
-        visitor.visit(self.root_node)
-        return visitor.imports
-
-    def _remove_nodes_from_root(self, nodes):
-        for node in nodes:
-            self.root_node.body.remove(node)
-
-    def __call__(self):
-        visitor = MyVisitor(self.root_node)
-        imports = self._collect_imports(visitor)
-        self._remove_nodes_from_root(
-            [import_.node for import_ in imports])
-
-        print ast.dump(self.root_node)
+from rope.base.project import Project
+from rope.base.resources import File
+from rope.refactor.importutils import ImportOrganizer
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d', '--dry-run', dest='dry_run', action='store_true',
+        help='If set, only preview the changes',
+    )
     parser.add_argument('target')
     args = parser.parse_args()
 
     target_file = args.target
 
-    with open(target_file) as f:
-        ImportOrganizer(f)()
+    project = Project(os.getcwd())
+    import_organizer = ImportOrganizer(project)
+    resource = File(project, target_file)
+    changeset = import_organizer.organize_imports(resource)
+
+    if args.dry_run:
+        print changeset.get_description()
+    else:
+        project.do(changeset)
