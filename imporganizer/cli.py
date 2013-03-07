253b1c84320990f5d54ec54135a67301a5a0c39c
Kevin Qiu <kevin@freshbooks.com>
2013-03-06 23:35:59 -0500

    Refactor

--- imporganizer/cli.py
+++ imporganizer/cli.py
@@ -4,6 +4,41 @@ import os
 from rope.base.project import Project
 from rope.base.resources import File
 from rope.refactor.importutils import ImportOrganizer
+from rope.refactor.wrap_line import WrapLine
+
+
+class Command(object):
+    def __init__(self, project, target_file, dry_run):
+        self.project = project
+        self.target_file = target_file
+        self.dry_run = dry_run
+
+    @property
+    def resource(self):
+        return File(self.project, self.target_file)
+
+    def __call__(self):
+        changeset = self.get_changeset()
+        if changeset is not None:
+            if self.dry_run:
+                print changeset.get_description()
+            else:
+                self.project.do(changeset)
+
+    def get_changeset(self):
+        raise NotImplementedError
+
+
+class OrganizeImportCommand(Command):
+    def get_changeset(self):
+        import_organizer = ImportOrganizer(self.project)
+        return import_organizer.organize_imports(self.resource)
+
+
+class WrapLineCommand(Command):
+    def get_changeset(self):
+        line_wrapper = WrapLine(self.project, self.resource)
+        return line_wrapper.get_changes()
 
 
 def main():
@@ -16,16 +51,7 @@ def main():
     args = parser.parse_args()
 
     target_file = args.target
-
     project = Project(os.getcwd())
-    import_organizer = ImportOrganizer(project)
-    resource = File(project, target_file)
-    changeset = import_organizer.organize_imports(resource)
 
-    if changeset is not None:
-        if args.dry_run:
-            print changeset.get_description()
-        else:
-            project.do(changeset)
-    else:
-        print 'No changes'
+    OrganizeImportCommand(project, target_file, args.dry_run)()
+    WrapLineCommand(project, target_file, args.dry_run)()
